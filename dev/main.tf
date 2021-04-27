a4501ea83cd85a6e6e52ea7353aa5fe2a48de7ea
onadkarni <omkar.nadkarni1@gmail.com>
2021-04-27 04:45:48 +0000

    post adding modules to dev and prod

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,5 @@
 module "webservers" {
-    source = "/home/omkar_nadkarni1/learn/class1-gcp/modules/compute"
+    source = "github.com/onadkarni/gcp-test-modules/compute"
     projectid = var.projectid
     computer_name = var.computer_name
     service_account_compute_emailid = var.service_account_compute_emailid
