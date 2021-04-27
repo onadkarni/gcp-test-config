a47f9649335f38f8c834de3cff52e11cfddca90a
onadkarni <omkar.nadkarni1@gmail.com>
2021-04-27 04:52:36 +0000

    adding comments to dev/main

--- dev/main.tf
+++ dev/main.tf
@@ -3,4 +3,5 @@ module "webservers" {
     projectid = var.projectid
     computer_name = var.computer_name
     service_account_compute_emailid = var.service_account_compute_emailid
+    #adding this comment into the new branch
 }
