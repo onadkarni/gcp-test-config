2c883d0bc45490f4ccc67fcc259df3036b2a02d2
onadkarni <omkar.nadkarni1@gmail.com>
2021-04-27 05:00:58 +0000

    fix broken issue hotfix

--- dev/main.tf
+++ dev/main.tf
@@ -4,4 +4,5 @@ module "webservers" {
     computer_name = var.computer_name
     service_account_compute_emailid = var.service_account_compute_emailid
     #adding this comment into the new branch
+   #added hotfix issue fix 
 }
